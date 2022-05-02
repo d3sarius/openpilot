fd1e923d0ee9572fecefe40deef83bdb25338393
d3sarius <jstarko@gmail.com>
2022-05-02 12:14:25 -0500

    Updated Default Tunes

--- selfdrive/car/chrysler/tunes.py
+++ selfdrive/car/chrysler/tunes.py
@@ -28,16 +28,16 @@ def set_lat_tune(tune, name, MAX_LAT_ACCEL=3.5, FRICTION=.001):
       tune.indi.actuatorEffectivenessV = [1.0]
     elif 'PID' in str(name):
       tune.init('pid')
-      tune.pid.kiBP = [0.0]
-      tune.pid.kpBP = [0.0]
+      tune.pid.kiBP = [0.0, 25.,]
+      tune.pid.kpBP = [0.0, 25.,]
       if name == LatTunes.PID_A:
-        tune.pid.kpV = [0.2]
-        tune.pid.kiV = [0.05]
-        tune.pid.kf = 0.00003
-      elif name == LatTunes.PID_B:
-        tune.pid.kpV = [0.6]
-        tune.pid.kiV = [0.1]
+        tune.pid.kpV = [0.10, 0.10,]
+        tune.pid.kiV = [0.008, 0.008,]
         tune.pid.kf = 0.00006
+      elif name == LatTunes.PID_B:
+        tune.pid.kpV = [0.1, 0.12,]
+        tune.pid.kiV = [0.0001, 0.0001,]
+        tune.pid.kf = 0.000018
       else:
           raise NotImplementedError('This PID tune does not exist')
     else:
